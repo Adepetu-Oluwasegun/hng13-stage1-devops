8802f1340bc538fc9312852a711c091bae8712b0
Oluwasegun-Adepetu <Oluwasegunadepetu@gmail.com>
2025-10-21 14:35:12 +0100

    update

--- deploy.sh
+++ deploy.sh
@@ -107,7 +107,7 @@ server {
     server_name $DOMAIN;
 
     location / {
-        proxy_pass http://127.0.0.1:$APP_PORT;
+        proxy_pass http://127.0.0.1:5000;
         proxy_set_header Host \$host;
         proxy_set_header X-Real-IP \$remote_addr;
         proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
